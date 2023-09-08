9ec50de4733c4c748d85eae658fcadc6b2f70149
ParkDongho <parkdongho28@naver.com>
2023-09-08 17:01:45 +0900

    Disable rust_tools

--- plugins/user.lua
+++ plugins/user.lua
@@ -190,15 +190,15 @@ return {
     end,
   },
 
-  {
-    "simrat39/rust-tools.nvim", -- add lsp plugin
-    {
-      "williamboman/mason-lspconfig.nvim",
-      opts = {
-        ensure_installed = { "rust_analyzer" },
-      },
-    },
-  },
+  -- {
+  --   "simrat39/rust-tools.nvim", -- add lsp plugin
+  --   {
+  --     "williamboman/mason-lspconfig.nvim",
+  --     opts = {
+  --       ensure_installed = { "rust_analyzer" },
+  --     },
+  --   },
+  -- },
 
   {
     "catppuccin/nvim",
